a08944cbde811417fcb0a2018b2cf07f7c8a4209
Akshat <masterakshata@gmail.com>
2021-02-06 04:01:30 +0530

    responsove menu and fix links

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 document.querySelectorAll('.smart-input input,.smart-input label').forEach(el =>{
-    console.log(el);
     el.addEventListener('click',(ev)=>{
         if(ev.target.value) ev.target.classList.add('filled')
     })
+})
+
+const hamburger = document.querySelectorAll('.burger')[0]
+const menu = document.querySelectorAll('nav ul')[0]
+const menu_lines = document.querySelectorAll('.burger .line') 
+let toggle = false
+hamburger.addEventListener('click',()=>{
+    menu.classList.toggle('open')
+    if(!toggle){
+        menu_lines[0].style.transform = 'rotate(45deg)'
+        menu_lines[1].style.transform = 'rotate(-45deg)'
+        menu_lines[2].style.transform = 'rotate(-90deg)'
+    }else{
+        menu_lines[0].style.transform = 'rotate(0deg)'
+        menu_lines[1].style.transform = 'rotate(0deg)'
+        menu_lines[2].style.transform = 'rotate(0deg)'
+    }
+    toggle = !toggle
+    
 })
