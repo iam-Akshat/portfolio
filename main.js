2ffa973d3fa71902042c2840df91016cd5754dfa
Akshat <masterakshata@gmail.com>
2021-02-07 02:06:24 +0530

    add nav menu animation on scroll to different sections

--- main.js
+++ main.js
@@ -22,3 +22,39 @@ hamburger.addEventListener('click',()=>{
     toggle = !toggle
     
 })
+const removeActiveFromOtherLinks = (exceptThisLink) => {
+    const all_links = [
+        'l-1',
+        'l-2',
+        'l-3',
+        'l-4',
+    ]
+    all_links.forEach(link=>{
+        if(link!==exceptThisLink){
+            const notEl = document.querySelector('li.'+link)
+            if(notEl.classList.contains('active')){
+                notEl.classList.remove('active')
+            }
+        }
+    })
+}
+const observerOptions = {
+    // rootMargin: '0px 0px 50% 0px',
+    threshold: 0.33
+}
+const ib = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry => {
+        // based on assumption that last name in classList always related link number
+        const section_number = entry.target.classList[entry.target.classList.length-1];
+        if(entry.isIntersecting){
+            removeActiveFromOtherLinks(section_number)
+            document.querySelector('li.'+section_number).classList.add('active')
+            
+        }
+    })
+
+},observerOptions)
+
+document.querySelectorAll('section').forEach(section=>{
+    ib.observe(section)
+})
